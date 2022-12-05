4d7c23621fb1de806b7a6143a394cfe6e5401e94
Eitan Reiffman <eitanreiffman@gmail.com>
2022-12-05 11:04:39 -0500

    Added title

--- ct-prework.py
+++ ct-prework.py
@@ -64,6 +64,7 @@ is_leap_year(chosen_year)
 
 """
 
+# Question 5
 # Write a function to check to see if all numbers in list are consecutive numbers.
 # For example, [2,3,4,5,6,7] are consecutive numbers,
 # but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
